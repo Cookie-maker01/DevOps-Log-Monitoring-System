0e95be632c51a233a9f6b81b8273ded1f9f8a326
unknown <weifang060@gmail.com>
2026-04-16 16:02:44 +1200

    Modify dashboard file

--- src/dashboard.py
+++ src/dashboard.py
@@ -14,28 +14,58 @@ def load_report():
 def home():
 
     return """
-    <html>
-    <head>
-        <title>DevOps Dashboard</title>
-        <style>
-            body { font-family: Arial; margin: 40px; }
-            .card { padding: 20px; margin: 10px; background: #f4f4f4; }
-        </style>
-    </head>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DevOps Dashboard</title>
+    <script src="https://cdn.jsdeliver.net/npm/chart.js"></script>
+</head>
 
-    <body>
-        <h1>DevOpes Log Dashboard</h1>
+<body>
+    <h1>DevOpes Log Dashboard</h1>
 
-        <div class="card">
-            <p>Open API: <a href="/api/report">/api/report</a></p>
-        </div>
-      </body>
-      </html>
-      """
+    <canvas id="errorChart" width="400" height="200"></canvas>
+
+    <script>
+
+        async function loadData() {
+            const res = await fetch('/api.report');
+            return await res.json();
+        }
+
+        async function renderChart() {
+        
+            const data = await loadData();
+            
+            const ctx = document.getElementById('errorChart').getContext('2d');
+
+            new Chart(ctx, {
+                type: 'line',
+                data:{
+                    labels: ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
+                    datasets: [{
+                        label: 'Error Trend',
+                        data: data.error_trend,
+                        borderColor: 'red'
+                    }]
+                }
+            });
+        }
+
+        renderChart();
+
+      </script>
+
+  </body>
+</html>
+"""
 
 @app.route("/api/report")
 def api_report():
-    return jsonify(load_report())
+    data = load_report()
+
+    data["error_trend"] = [0, 1 , 0, 2, 1, 3, 2]
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
